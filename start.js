1110754e77377b54c3aca7efcb9d36d4a5b4da0d
xwiz <c.opata@robosyslive.com>
2022-12-17 04:24:42 -0800

    Update start.js

--- start.js
+++ start.js
@@ -1,61 +1,38 @@
-const axios = require('axios');
+const fetch = require('node-fetch');
 
-async function extractDesignElements(fileId, accessToken) {
-  // Retrieve the node tree of the file
-  const response = await axios.get(
-    `https://api.figma.com/v1/files/${fileId}/nodes`,
+const API_KEY = 'YOUR_API_KEY';
+const FILE_ID = 'YOUR_FILE_ID';
+const PAGE_NAME = 'YOUR_PAGE_NAME';
+
+async function extractDesignElements() {
+  // Retrieve the node tree of the mobile screen
+  const response = await fetch(
+    `https://api.figma.com/v1/files/${FILE_ID}/nodes`,
     {
       headers: {
-        'X-Figma-Token': accessToken,
+        'X-Figma-Token': API_KEY,
       },
     }
   );
-  const nodes = response.data.nodes;
+  const data = await response.json();
 
-  // Traverse the node tree and extract the design elements
+  // Find the page node with the specified name
+  const pageNode = data.nodes.find(
+    node => node.name === PAGE_NAME && node.type === 'PAGE'
+  );
+
+  // Extract the design elements from the page node
   const designElements = [];
-  for (const nodeId in nodes) {
-    const node = nodes[nodeId];
-    if (node.type === 'TEXT') {
-      // Extract text element
-      designElements.push({
-        type: 'text',
-        text: node.characters,
-        fontName: node.fontName.family,
-        fontSize: node.fontSize,
-        color: node.fills[0].color,
-      });
-    } else if (node.type === 'RECTANGLE') {
-      // Extract shape element
-      designElements.push({
-        type: 'shape',
-        color: node.fills[0].color,
-      });
-    } else if (node.type === 'VECTOR') {
-      // Extract image element
-      const imageUrl = await getVectorImageUrl(node.id, accessToken);
-      designElements.push({
-        type: 'image',
-        url: imageUrl,
-      });
+  function extractElements(node) {
+    if (node.type === 'TEXT' || node.type === 'SHAPE' || node.type === 'IMAGE') {
+      designElements.push(node);
+    } else if (node.type === 'GROUP' || node.type === 'FRAME') {
+      node.children.forEach(child => extractElements(child));
     }
   }
+  extractElements(pageNode);
 
   return designElements;
 }
 
-async function getVectorImageUrl(nodeId, accessToken) {
-  // Retrieve the vector image data for the node
-  const response = await axios.get(
-    `https://api.figma.com/v1/images/${nodeId}`,
-    {
-      headers: {
-        'X-Figma-Token': accessToken,
-      },
-      params: {
-        format: 'png',
-      },
-    }
-  );
-  return response.data.url;
-}
+extractDesignElements().then(elements => console.log(elements));
