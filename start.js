fb19e85de730f29d5b87977e7d31dc38aa0eeda9
xwiz <c.opata@robosyslive.com>
2022-12-17 04:35:39 -0800

    Update start.js

--- start.js
+++ start.js
@@ -22,14 +22,56 @@ async function extractDesignElements() {
   );
 
   // Extract the design elements from the page node
+  const screenLinks = [];
   const designElements = [];
-  function extractElements(node) {
-    if (node.type === 'TEXT' || node.type === 'SHAPE' || node.type === 'IMAGE') {
-      designElements.push(node);
-    } else if (node.type === 'GROUP' || node.type === 'FRAME') {
-      node.children.forEach(child => extractElements(child));
-    }
+function extractElements(node) {
+  if (node.type === 'TEXT' || node.type === 'SHAPE' || node.type === 'IMAGE') {
+    // Extract the type, name, style, and other relevant properties of the element
+    designElements.push({
+      type: node.type,
+      name: node.name,
+      color: node.fills[0].color,
+      fontSize: node.style.fontSize,
+      fontWeight: node.style.fontWeight,
+      textAlign: node.style.textAlign,
+      textDecoration: node.style.textDecoration,
+      textTransform: node.style.textTransform,
+      borderColor: node.strokes[0].color,
+      borderWidth: node.strokes[0].weight,
+      borderRadius: node.cornerRadius,
+      imageUrl: node.type === 'IMAGE' ? node.images[0].url : null,
+    });
+  } else if (node.type === 'GROUP' || node.type === 'FRAME') {
+    // Extract the layout and positioning properties of the element
+    designElements.push({
+      type: node.type,
+      name: node.name,
+      width: node.width,
+      height: node.height,
+      margin: node.absoluteBoundingBox.x + ' ' + node.absoluteBoundingBox.y,
+      padding: node.relativeTransform[0][2] + ' ' + node.relativeTransform[1][2],
+      flexDirection: node.layoutMode === 'HORIZONTAL' ? 'row' : 'column',
+      justifyContent: node.layoutAlign === 'MIN' ? 'flex-start' : 'center',
+      alignItems: node.layoutAlign === 'MIN' ? 'flex-start' : 'center',
+    });
+    
+    if (node.links.length > 0) {
+      for (var link in node.links)  {
+      // Extract the link destination ID and type (SCREEN or PAGE)
+          screenLinks.push({
+            nodeName: node.name,
+            link: link,
+            linkId: link.url.split('/')[5],
+            linkType = link.url.split('/')[4]
+          });
+       }
+     }
+
+    // Recursively extract the elements inside the group or frame
+    node.children.forEach(child => extractElements(child));
   }
+}
+  
   extractElements(pageNode);
 
   return designElements;
